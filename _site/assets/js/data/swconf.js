ec2d2b3fc13c27fa8cee654b36187e5d50264d41
Leonam Ricardo Vasquez Ramos <leonamvasquezr@gmail.com>
2025-12-16 23:36:48 -0300

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758757619',resources: [
+    cacheName: 'chirpy-1765935668',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
